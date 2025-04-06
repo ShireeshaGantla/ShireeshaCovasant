6c7a3c2706082a87afb931c583a80ed4f3fd7056
ShireeshaGantla <shirishareddygantla8@gmail.com>
2025-04-06 20:39:23 +0530

    Add files via upload

---
+++ assignment_day2.1.py
@@ -0,0 +1,20 @@
+import glob
+import os.path
+
+url = r"C:\Users\Shireesha\handson"
+def largestFile(url, max_size=0, largest_file=""):
+    for item in glob.glob(os.path.join(url, "*")):
+        if os.path.isfile(item):
+            size = os.path.getsize(item)
+            if size > max_size:
+                max_size = size
+                largest_file = item
+            
+        elif os.path.isdir(item):
+            max_size, largest_file = largestFile(item, max_size, largest_file)
+    return max_size, largest_file
+
+if __name__ == "__main__":
+    max_file, large_file_path = largestFile(url)
+    print(max_file, large_file_path)
+
